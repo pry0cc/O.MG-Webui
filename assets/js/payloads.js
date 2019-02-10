c075b973ce73283f8d8075a61ad7b33294a7a6fa
pry0cc <pry0cc@protonmail.com>
2019-02-10 18:24:06 +0000

    Added settings modals

--- assets/js/payloads.js
+++ assets/js/payloads.js
@@ -2,17 +2,17 @@ var payloads = [
 	{
 		"name":"MacOS Reverse Shell",
 		"id":"macos/terminal/reverse_shell",
-		"code":"Something something something"
+		"code":"This is the code for the MacOS reverse shell"
 	},
 	{
 		"name":"Ubuntu Reverse Shell",
 		"id":"ubuntu/terminal/reverse_shell",
-		"code":"Something something something"
+		"code":"This is the code for the ubuntu reverse shell"
 	},
 	{
 		"name":"Windows Reverse Shell",
 		"id":"windows/powershell/reverse_shell",
-		"code":"Something something something Windows"
+		"code":"This is the code for the windows powershell reverse shell"
 	},
 	{
 		"name":"Visit 0x00sec.org - Windows",
